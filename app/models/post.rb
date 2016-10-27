235e3541be836402a6d17f6ab8be3ca756d31d48
gyeon <gyeon19@gmail.com>
2016-10-27 15:58:44 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbaity
+
+  def clickbaity
+    statements = ["Won't Believe", "Secret", "Guess"]
+    if self.title
+      if statements.none? {|click| self.title.include?(click)}
+        errors.add(:title, "clickbait")
+      end
+    end
+  end
+
+
+
 end
